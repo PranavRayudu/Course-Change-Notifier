88995828c7d8ce63352fe754411e8889d42a07fe
Pranav Rayudu <tarakapranav@gmail.com>
2020-07-06 21:53:06 -0500

    fixed parsing bugs and updated README.md to reflect true usage

--- README.md
+++ README.md
@@ -14,16 +14,27 @@ Chrome drivers are available [here](https://chromedriver.chromium.org/downloads)
 You will need a Twillio account to send SMS messages to your phone when a course opens up.
 > section in progress
 
-If you have the UT Schedule extension, auto-load may need to be turned off. 
-This also requires user to be logged into their UT_ID and DUO (with remember for 30 days) beforehand.
-To configure your project, create a ``.env`` file and add the following data
-To run the project, simply run ``python course-monitor.py -link=<link of page> --uid 12345 101010``
+having the UT Registration Plus extension does not affect this script.
+This also requires user to be log into their UT_ID and DUO when it starts up. Script will continue executing once you reach the course schedule.
+To configure your project for Twillio, create a ``.env`` file and add the following data
+```.env
+
+```
+otherwise, use the ConsoleEmitter (prints to console) by commenting out Twillio's import statement and initializer.
+
+To run the project, simply run ``python course-monitor.py -link=<link of page> --uid <uid of course 1> <uid of course 2>``
 The ``-link`` argument is required and takes in the url of the first page of results of the courses the script should monitor
-The ``--uid`` argument is optional and takes in a space separated list of course unique ids to keep track of. If you do not use this, then all courses will be monitored.
+The ``--uid`` argument is optional (None by default) and takes in a space separated list of course unique ids to keep track of. If you do not use this, all courses on the page will be monitored.
+The ``--debug`` argument is optional (False by default) and enables printing data to the console. It is recommended you keep this on.
+
+Example usage
+```commandline
+python course_monitor.py -link https://utdirect.utexas.edu/apps/registrar/course_schedule/20209/results/?flags=CULTDIVR&search_type_main=CORE&ccyys=20209&fos_fl=&level=&instr_last_name=&instr_first_initial=&fos_cn=&course_number=&start_unique=&end_unique=&mtg_days_st=000000&mtg_start_time_st=00&core_code=060 --uid 37965 --debug
+```
 
 ## Todo
-- [ ] Add course comparision
 - [ ] Add messaging to user (Twillio API)
 
 ## Contribution
 This script is an open-source project that relies on its users to get better. If you would like to add features, please fork and create a PR.
+If you would like to add your own notification method, simply extent the NotificationEmitter class and override the constructor and emit() function.
--- course_monitor.py
+++ course_monitor.py
@@ -10,7 +10,8 @@ from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.common.exceptions import NoSuchElementException
 
-from notification_emitter import ConsoleEmitter
+# from notification_emitter import ConsoleEmitter
+from notification_emitter import TwillioMsgEmitter
 
 # how long should selenium wait for an element to appear...
 implicit_wait_time = 30
@@ -18,10 +19,22 @@ implicit_wait_time = 30
 random_params = (30, 40)
 random_time = random.randrange
 
-emitter = ConsoleEmitter()
+debug = False
+
+# emitter = ConsoleEmitter()
+emitter = TwillioMsgEmitter()
+
+
+def d_print(msg):
+    """Prints status messages only if debug flag is used"""
+    if debug:
+        print(msg)
 
 
 def init_browser_link(link: str):
+    """Starts browser and navigates to course schedule"""
+    d_print('browser going to {} (you may need to sign in)'.format(link))
+
     driver = webdriver.Chrome()  # todo change this to support any browser
     driver.implicitly_wait(implicit_wait_time)
     driver.get(link)
@@ -29,6 +42,8 @@ def init_browser_link(link: str):
     # wait for user logs in and the courses can be seen
     WebDriverWait(driver, sys.maxsize) \
         .until(lambda x: 'UT Austin Registrar:' in driver.title and 'course search' in driver.title)
+
+    d_print('successfully entered course schedule')
 
     return driver
 
@@ -53,12 +68,33 @@ def parse_courses(driver) -> dict:
             status = row.find('td', {'data-th': 'Status'})
 
             if header:
-                curr_header = header
+                curr_header = header.text.strip()
             else:
                 assert not header
-                courses[unique] = (curr_header, status)
+                courses[unique.text] = (curr_header, status.text)
+    else:
+        d_print('did not find table in course schedule. What is going on?')
 
+    d_print('here are the courses I found')
+    d_print(courses)
     return courses
+
+
+def click_next(driver) -> bool:
+    """Returns true if a next button exists and was clicked, false otherwise"""
+
+    success = False
+    try:
+        driver.implicitly_wait(0)
+        driver.find_element_by_id('next_nav_link').click()
+        success = True
+    except NoSuchElementException:
+        pass
+    finally:
+        driver.implicitly_wait(implicit_wait_time)
+
+        d_print('found another page and clicking it!' if success else 'this is the last page of courses')
+        return success
 
 
 def filter_courses(courses: dict, uids: list) -> dict:
@@ -72,6 +108,12 @@ def filter_courses(courses: dict, uids: list) -> dict:
     for uid in uids:
         if uid in courses:
             filtered_courses[uid] = courses[uid]
+
+    d_print('we only care about courses with uids: {}'.format(uids))
+    d_print('and the filtered list is {}'.format(filtered_courses))
+
+    if len(filtered_courses) == 0:
+        d_print('No course that we want has been found in course list. Are you sure you are on the right course page?')
 
     return filtered_courses
 
@@ -88,24 +130,14 @@ def changelist(p_courses: dict, c_courses: dict) -> dict:
             if p_status != status:
                 changed_courses[uid] = (name, p_status, status)
         else:
-            print('lost {} ({}) from refreshed course list'.format(name, uid))
+            d_print('lost {} ({}) from refreshed course list'.format(name, uid))
 
+    if len(changed_courses) > 0:
+        d_print('list of courses that changed statuses')
+        d_print(changed_courses)
+    else:
+        d_print('no change in courses')
     return changed_courses
-
-
-def click_next(driver) -> bool:
-    """Returns true if a next button exists and was clicked, false otherwise"""
-
-    success = False
-    try:
-        driver.implicitly_wait(0)
-        driver.find_element_by_id('next_nav_link').click()
-        success = True
-    except NoSuchElementException:
-        pass
-    finally:
-        driver.implicitly_wait(implicit_wait_time)
-        return success
 
 
 if __name__ == '__main__':
@@ -114,20 +146,29 @@ if __name__ == '__main__':
                         metavar='<href>',
                         type=str,
                         required=True,
-                        help='the link with the courses here')
+                        help='the url of the course schedule search results')
 
     parser.add_argument('--uid',
-                        metavar='uid uid uid...',
+                        metavar='id',
                         type=int,
                         required=False,
                         nargs="+",
                         help='space separated list of the unique IDs of courses we are interested in searching')
 
+    parser.add_argument('--debug',
+                        required=False,
+                        action='store_true',
+                        help='add this flag to see debug / status prints')
+
     args = parser.parse_args()
 
     load_dotenv(os.path.join('./', '.env'))
 
+    debug = args.debug is not None
+
     browser = init_browser_link(args.link)
+
+    uid = [str(uid) for uid in args.uid]
 
     prev_courses = None
     curr_courses = None
@@ -144,7 +185,7 @@ if __name__ == '__main__':
                 break
 
         # only keep the courses we are interested in
-        curr_courses = filter_courses(curr_courses, args.uid)
+        curr_courses = filter_courses(curr_courses, uid)
 
         if prev_courses:
             changed_courses = changelist(prev_courses, curr_courses)
@@ -153,4 +194,6 @@ if __name__ == '__main__':
                 emitter.emit_msg(changed_courses)
 
         prev_courses = curr_courses
-        time.sleep(random_time(*random_params))
+        sleep_time = random_time(*random_params)
+        d_print('going to sleep for {} seconds'.format(sleep_time))
+        time.sleep(sleep_time)
